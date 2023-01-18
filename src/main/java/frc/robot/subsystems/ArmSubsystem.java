fd193e388037c2bd6dc0f6c26c591826772b9ebf
Rio Blumenthal <rio@blumenthal.com>
2023-01-18 17:57:58 -0600

    arm subsystem 1.0

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,17 +4,19 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.utils.SparkMax;
 import frc.robot.utils.SparkPIDConfig;
 
-public class ArmSubsystem extends SubsystemBase {
+public class ArmSubsystem extends SubsystemBase implements AutoCloseable {
 
-  public static class Hardware{
+  public static class Hardware {
     private boolean isHardwareReal;
     private SparkMax pivotMasterMotor, pivotSlaveMotor;
     private SparkMax telescopeMasterMotor, telescopeSlaveMotor;
@@ -33,8 +35,23 @@ public class ArmSubsystem extends SubsystemBase {
     }
   }
 
+  public enum ArmState {
+    Top(Constants.Arm.TELESCOPE_CONFIG.getUpperLimit(), Constants.Arm.PIVOT_CONFIG.getUpperLimit()),
+    Bottom(Constants.Arm.TELESCOPE_CONFIG.getLowerLimit(), Constants.Arm.PIVOT_CONFIG.getLowerLimit());
+
+    public final double telescopePosition;
+    public final double pivotPosition;
+    private ArmState(double telescopePosition, double pivotPosition) {
+      this.telescopePosition = telescopePosition;
+      this.pivotPosition = pivotPosition;
+    }
+  }
+
   private SparkMax m_pivotMasterMotor, m_pivotSlaveMotor;
   private SparkMax m_telescopeMasterMotor, m_telescopeSlaveMotor;
+
+  private SparkPIDConfig m_pivotConfig;
+  private SparkPIDConfig m_telescopeConfig;
 
   /** Creates a new ArmSubsystem. */
   public ArmSubsystem(Hardware armHardware, SparkPIDConfig pivotConfig, SparkPIDConfig telescopeConfig) {
@@ -55,24 +72,53 @@ public class ArmSubsystem extends SubsystemBase {
     // Set pivot slave motor to follow master
     m_pivotSlaveMotor.follow(m_pivotMasterMotor);
 
-    // TODO feedback sensor
-    pivotConfig.initializeSparkPID(m_pivotMasterMotor, null);
-    telescopeConfig.initializeSparkPID(m_telescopeMasterMotor, null);
-    
+    // initialize PID
+    pivotConfig.initializeSparkPID(m_pivotMasterMotor, m_pivotMasterMotor.getEncoder());
+    telescopeConfig.initializeSparkPID(m_telescopeMasterMotor, m_telescopeMasterMotor.getEncoder());
   }
 
   public static Hardware initializeHardware(boolean isHardwareReal) {
-    Hardware drivetrainHardware = new Hardware(isHardwareReal,
+    Hardware armHardware = new Hardware(isHardwareReal,
                                                new SparkMax(Constants.ArmHardware.ARM_PIVOT_MASTER_ID, MotorType.kBrushless),
                                                new SparkMax(Constants.ArmHardware.ARM_PIVOT_SLAVE_ID, MotorType.kBrushless),
                                                new SparkMax(Constants.ArmHardware.ARM_TELESCOPE_MASTER_ID, MotorType.kBrushless),
                                                new SparkMax(Constants.ArmHardware.ARM_TELESCOPE_SLAVE_ID, MotorType.kBrushless));
-
-    return drivetrainHardware;
+    return armHardware;
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+  public void setPivotPosition(double setpoint) {
+    // Put setpoint within the bounds of the motor
+    setpoint = MathUtil.clamp(setpoint, m_pivotConfig.getLowerLimit(), m_pivotConfig.getUpperLimit());
+
+    // Set pivot motor
+    m_pivotMasterMotor.set(setpoint, ControlType.kPosition);
+  }
+
+  public void setTelescopePosition(double setpoint) {
+    // Put setpoint within the bounds of the motor
+    setpoint = MathUtil.clamp(setpoint, m_telescopeConfig.getLowerLimit(), m_telescopeConfig.getUpperLimit());
+
+    // Set telescope motor
+    m_telescopeMasterMotor.set(setpoint, ControlType.kPosition);
+  }
+
+  public void setArmState(ArmState armState) {
+    // Set telescope and pivot positions from enum
+    setPivotPosition(armState.pivotPosition);
+    setTelescopePosition(armState.telescopePosition);
+  }
+
+  @Override
+  public void close() {
+    m_telescopeMasterMotor.close();
+    m_telescopeSlaveMotor.close();
+    m_pivotMasterMotor.close();
+    m_pivotSlaveMotor.close();
+  }
+
 }
