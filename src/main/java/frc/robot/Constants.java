9ea8fcfd56a45951533c65d67e28e12467f0ce25
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2023-01-19 15:11:39 -0600

    Remove redundant variables

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -70,14 +70,4 @@ public final class Constants {
     public static final String SMARTDASHBOARD_DEFAULT_TAB = "SmartDashboard";
     public static final String SMARTDASHBOARD_AUTO_MODE = "Auto Mode";
   }
-  public static class VisionConstants {
-    public static final Transform3d robotToCam =
-            new Transform3d(
-                    new Translation3d(0.5, 0.0, 0.5),
-                    new Rotation3d(
-                            0, 0,
-                            0)); // Cam mounted facing forward, half a meter forward of center, half a meter up
-    // from center.
-    public static final String cameraName = "forward_camera";
-}
 }
