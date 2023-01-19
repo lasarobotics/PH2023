7f358b14db4756447b8789ad7cb734c8fe5ff17f
hebounds <hayeservin@gmail.com>
2023-01-19 17:20:49 -0600

    Added object sensing/color reading

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -12,30 +12,46 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.utils.SparkMax;
 
-public class IntakeSubsystem extends SubsystemBase {
+public class IntakeSubsystem extends SubsystemBase implements AutoCloseable {
   public static class Hardware {
     private boolean isHardwareReal;
     private SparkMax wristMotor;
     private SparkMax rollerMotor;
+    private ColorSensorV3 m_colorSensor;
 
     public Hardware(boolean isHardwareReal,
                     SparkMax wristMotor, 
-                    SparkMax rollerMotor) {
+                    SparkMax rollerMotor,
+                    ColorSensorV3 m_colorSensor) {
       this.isHardwareReal = isHardwareReal;
       this.wristMotor = wristMotor;
       this.rollerMotor = rollerMotor;
+      this.m_colorSensor = m_colorSensor;
+    }
+  }
+
+  public enum GameObject {
+    Cone(new Color(0.0, 0.0, 0.0)), Cube(new Color(0.0, 0.0, 0.0)); // TO-DO: Change color targets to be accurate
+    
+    public Color color;
+
+    public GameObject(Color objectColor) {
+      color = objectColor;
     }
   }
 
   private final SparkMax m_wristMotor;
   private final SparkMax m_rollerMotor;
 
+  private final ColorMatch m_colorMatcher;
 
   /**
    * Create a new intake subsystem
    * @param intakeHardware Intake hardware
    */
   public IntakeSubsystem(Hardware intakeHardware) {
+
+    this.m_colorMatcher = new ColorMatch();
     this.m_wristMotor = intakeHardware.wristMotor;
     this.m_rollerMotor = intakeHardware.rollerMotor;
 
@@ -46,12 +62,16 @@ public class IntakeSubsystem extends SubsystemBase {
     // Set motors to break
     m_wristMotor.setIdleMode(IdleMode.kBrake);
     m_rollerMotor.setIdleMode(IdleMode.kBrake);
+
+    m_colorMatcher.addColorMatch(yellowTarget);
+    m_colorMatcher.addColorMatch(purpleTarget);
   }
 
   public static Hardware initializeHardware(boolean isHardwareReal) {
     Hardware intakeHardware = new Hardware(isHardwareReal,
                                            new SparkMax(Constants.IntakeHardware.WRIST_MOTOR_ID, MotorType.kBrushless),
-                                           new SparkMax(Constants.IntakeHardware.ROLLER_MOTOR_ID, MotorType.kBrushless));
+                                           new SparkMax(Constants.IntakeHardware.ROLLER_MOTOR_ID, MotorType.kBrushless),
+                                           new ColorSensorV3(I2C.Port.kOnboard)); // TO-DO: Change to correct port
     return intakeHardware;
   }
 
@@ -70,9 +90,48 @@ public class IntakeSubsystem extends SubsystemBase {
   }
 
   /**
+   * Identify game object
+   * @return GameObject, a Cone or Cube
+   */
+  public GameObject identifyObject() {
+    Color detectedColor = m_colorMSensor.getColor();
+
+    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+
+    if (match.color == GameObject.Cone.color) {
+      return GameObject.Cone;
+    }
+    else if (match.color == GameObject.Cube.color) {
+      return GameObject.Cube;
+    }
+    else {
+      return null;
+    }
+  }
+
+  /**
+   * Read and return current color
+   * @return Color, currently detected color by sensor
+   */
+  public Color readColor() {
+    Color detectedColor = m_colorSensor.getColor();
+
+    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+
+    return match.color;
+  }
+
+  /**
    * Stops motor
    */
   public void stop() {
     m_rollerMotor.stopMotor();
+  }
+
+  @Override
+  public void close() {
+    m_wristMotor.close();
+    m_rollerMotor.close();
+    m_colorSensor.close();
   }
 }
