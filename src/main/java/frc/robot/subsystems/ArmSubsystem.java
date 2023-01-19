85cb3ce7eb1d9557911a8aa17b611089a1bd89ea
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2023-01-19 15:35:40 -0600

    Add arm direction

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxAnalogSensor.Mode;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -25,8 +26,7 @@ public class ArmSubsystem extends SubsystemBase implements AutoCloseable {
                     SparkMax pivotMasterMotor, 
                     SparkMax pivotSlaveMotor, 
                     SparkMax telescopeMasterMotor,
-                    SparkMax telescopeSlaveMotor
-                    ) {
+                    SparkMax telescopeSlaveMotor) {
       this.isHardwareReal = isHardwareReal;
       this.pivotMasterMotor = pivotMasterMotor;
       this.pivotSlaveMotor = pivotSlaveMotor;
@@ -35,15 +35,53 @@ public class ArmSubsystem extends SubsystemBase implements AutoCloseable {
     }
   }
 
-  public enum ArmState {
-    Top(Constants.Arm.TELESCOPE_CONFIG.getUpperLimit(), Constants.Arm.PIVOT_CONFIG.getUpperLimit()),
-    Bottom(Constants.Arm.TELESCOPE_CONFIG.getLowerLimit(), Constants.Arm.PIVOT_CONFIG.getLowerLimit());
+  private enum ArmDirection {
+    Forward(+1),
+    Reverse(-1);
 
-    public final double telescopePosition;
-    public final double pivotPosition;
-    private ArmState(double telescopePosition, double pivotPosition) {
-      this.telescopePosition = telescopePosition;
-      this.pivotPosition = pivotPosition;
+    public final int value;
+    private ArmDirection(int value) {
+      this.value = value;
+    }
+  }
+
+  // TODO Set TelescopeState numbers
+  private enum TelescopeState {
+    Stowed(0.0),
+    Ground(0.0),
+    Middle(0.0),
+    High(0.0);
+
+    public final double value;
+    private TelescopeState(double value) {
+      this.value = value;
+    }
+  }
+
+  // TODO Set PivotState numbers
+  private enum PivotState {
+    Stowed(0.0),
+    Ground(0.0),
+    Middle(0.0),
+    High(0.0);
+
+    public final double value;
+    private PivotState(double value) {
+      this.value = value;
+    }
+  }
+
+  public enum ArmState {
+    Stowed(PivotState.Stowed, TelescopeState.Stowed),
+    Ground(PivotState.Ground, TelescopeState.Ground),
+    Middle(PivotState.Middle, TelescopeState.Middle),
+    High(PivotState.High, TelescopeState.High);
+
+    public final TelescopeState telescopeState;
+    public final PivotState pivotState;
+    private ArmState(PivotState pivotState, TelescopeState telescopeState) {
+      this.pivotState = pivotState;
+      this.telescopeState = telescopeState;
     }
   }
 
@@ -53,37 +91,81 @@ public class ArmSubsystem extends SubsystemBase implements AutoCloseable {
   private SparkPIDConfig m_pivotConfig;
   private SparkPIDConfig m_telescopeConfig;
 
-  /** Creates a new ArmSubsystem. */
+  private ArmDirection m_currentDirection;
+  private ArmState m_currentState;
+
+  /**
+   * Create an instance of ArmSubsystem
+   * <p>
+   * NOTE: ONLY ONE INSTANCE SHOULD EXIST AT ANY TIME!
+   * <p>
+   * @param armHardware Hardwave devices required by arm
+   * @param pivotConfig PID config for arm pivot
+   * @param telescopeConfig PID config for arm telescope
+   */
   public ArmSubsystem(Hardware armHardware, SparkPIDConfig pivotConfig, SparkPIDConfig telescopeConfig) {
     this.m_pivotMasterMotor = armHardware.pivotMasterMotor;
     this.m_pivotSlaveMotor = armHardware.pivotSlaveMotor;
     this.m_telescopeMasterMotor = armHardware.telescopeMasterMotor;
     this.m_telescopeSlaveMotor = armHardware.telescopeSlaveMotor;
 
-    // Set all drive motors to brake
+    m_currentDirection = ArmDirection.Forward;
+    m_currentState = ArmState.Stowed;
+
+    // Set all arm motors to brake
     m_pivotMasterMotor.setIdleMode(IdleMode.kBrake);
     m_pivotSlaveMotor.setIdleMode(IdleMode.kBrake);
     m_telescopeMasterMotor.setIdleMode(IdleMode.kBrake);
     m_telescopeSlaveMotor.setIdleMode(IdleMode.kBrake);
 
-    // Set telescope slave motor to follow master
-    m_telescopeSlaveMotor.follow(m_telescopeMasterMotor);
-
     // Set pivot slave motor to follow master
     m_pivotSlaveMotor.follow(m_pivotMasterMotor);
 
-    // initialize PID
-    pivotConfig.initializeSparkPID(m_pivotMasterMotor, m_pivotMasterMotor.getEncoder());
-    telescopeConfig.initializeSparkPID(m_telescopeMasterMotor, m_telescopeMasterMotor.getEncoder());
+    // Set telescope slave motor to follow master
+    m_telescopeSlaveMotor.follow(m_telescopeMasterMotor);
+
+    // Only do this stuff if hardware is real
+    if (armHardware.isHardwareReal) {
+      // initialize PID
+      pivotConfig.initializeSparkPID(m_pivotMasterMotor, m_pivotMasterMotor.getAlternateEncoder());
+      telescopeConfig.initializeSparkPID(m_telescopeMasterMotor, m_telescopeMasterMotor.getAnalog(Mode.kAbsolute));
+    }
   }
 
   public static Hardware initializeHardware(boolean isHardwareReal) {
     Hardware armHardware = new Hardware(isHardwareReal,
-                                               new SparkMax(Constants.ArmHardware.ARM_PIVOT_MASTER_ID, MotorType.kBrushless),
-                                               new SparkMax(Constants.ArmHardware.ARM_PIVOT_SLAVE_ID, MotorType.kBrushless),
-                                               new SparkMax(Constants.ArmHardware.ARM_TELESCOPE_MASTER_ID, MotorType.kBrushless),
-                                               new SparkMax(Constants.ArmHardware.ARM_TELESCOPE_SLAVE_ID, MotorType.kBrushless));
+                                        new SparkMax(Constants.ArmHardware.ARM_PIVOT_MASTER_ID, MotorType.kBrushless),
+                                        new SparkMax(Constants.ArmHardware.ARM_PIVOT_SLAVE_ID, MotorType.kBrushless),
+                                        new SparkMax(Constants.ArmHardware.ARM_TELESCOPE_MASTER_ID, MotorType.kBrushless),
+                                        new SparkMax(Constants.ArmHardware.ARM_TELESCOPE_SLAVE_ID, MotorType.kBrushless));
     return armHardware;
+  }
+
+  /**
+   * Set arm pivot position
+   * @param pivotState Desired pivot state
+   */
+  private void setPivotState(PivotState pivotState) {
+    // Put setpoint within the bounds of the motor
+    double setpoint = MathUtil.clamp(pivotState.value, m_pivotConfig.getLowerLimit(), m_pivotConfig.getUpperLimit());
+
+    // Account for arm direction
+    setpoint *= m_currentDirection.value;
+
+    // Set pivot motor
+    m_pivotMasterMotor.set(setpoint, ControlType.kPosition);
+  }
+
+  /**
+   * Set arm telescope position
+   * @param telescopeState Desired telescope state
+   */
+  private void setTelescopeState(TelescopeState telescopeState) {
+    // Put setpoint within the bounds of the motor
+    double setpoint = MathUtil.clamp(telescopeState.value, m_telescopeConfig.getLowerLimit(), m_telescopeConfig.getUpperLimit());
+
+    // Set telescope motor
+    m_telescopeMasterMotor.set(setpoint, ControlType.kPosition);
   }
 
   @Override
@@ -91,26 +173,50 @@ public class ArmSubsystem extends SubsystemBase implements AutoCloseable {
     // This method will be called once per scheduler run
   }
 
-  public void setPivotPosition(double setpoint) {
-    // Put setpoint within the bounds of the motor
-    setpoint = MathUtil.clamp(setpoint, m_pivotConfig.getLowerLimit(), m_pivotConfig.getUpperLimit());
-
-    // Set pivot motor
-    m_pivotMasterMotor.set(setpoint, ControlType.kPosition);
-  }
-
-  public void setTelescopePosition(double setpoint) {
-    // Put setpoint within the bounds of the motor
-    setpoint = MathUtil.clamp(setpoint, m_telescopeConfig.getLowerLimit(), m_telescopeConfig.getUpperLimit());
-
-    // Set telescope motor
-    m_telescopeMasterMotor.set(setpoint, ControlType.kPosition);
-  }
-
+  /**
+   * Set desired arm state
+   * @param armState Arm state, which includes pivot and telescope position
+   */
   public void setArmState(ArmState armState) {
-    // Set telescope and pivot positions from enum
-    setPivotPosition(armState.pivotPosition);
-    setTelescopePosition(armState.telescopePosition);
+    // Update current state
+    m_currentState = armState;
+
+    // Set telescope and pivot positions
+    setPivotState(m_currentState.pivotState);
+    setTelescopeState(m_currentState.telescopeState);
+  }
+
+  /**
+   * Get current arm state
+   * @return current state
+   */
+  public ArmState getArmState() {
+    return m_currentState;
+  }
+
+  /**
+   * Sets direction of arm to forward or reverse
+   * @param direction Arm direction
+   */
+  public void setArmDirection(ArmDirection direction) {
+    // Ignore if direction is already correct
+    if (direction == m_currentDirection) return;
+    
+    // If not, then change direction, and set to stowed position
+    m_currentDirection = direction;
+    setArmState(ArmState.Stowed);
+  }
+
+  /**
+   * Toggle arm direction
+   */
+  public void toggleArmDirection() {
+    // Toggle direction
+    if (m_currentDirection == ArmDirection.Forward) m_currentDirection = ArmDirection.Reverse;
+    else m_currentDirection = ArmDirection.Forward;
+
+    // Stow arm in new direction
+    setArmState(ArmState.Stowed);
   }
 
   @Override
@@ -120,5 +226,4 @@ public class ArmSubsystem extends SubsystemBase implements AutoCloseable {
     m_pivotMasterMotor.close();
     m_pivotSlaveMotor.close();
   }
-
 }
