61fce8fb2a41236aff31ccb2e1144353ec13ec6a
hebounds <hayeservin@gmail.com>
2023-02-27 19:38:38 -0600

    Event Map Constants

--- src/main/java/frc/robot/commands/autonomous/TestPath.java
+++ src/main/java/frc/robot/commands/autonomous/TestPath.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -24,13 +25,13 @@ public class TestPath extends SequentialCommandGroup {
   
   // construct a new auto command object
   public TestPath(DriveSubsystem driveSubsystem, IntakeSubsystem intakeSubsystem, ArmSubsystem armSubystem) {
-    eventMap.put("intake", new InstantCommand(() -> intakeSubsystem.intake()));
-    eventMap.put("outake", new InstantCommand(() -> intakeSubsystem.outake()));
-    eventMap.put("print", new PrintCommand("It has crossed the threshold"));
-    eventMap.put("stowed", new InstantCommand(() -> armSubystem.setArmState(ArmState.Stowed)));
-    eventMap.put("ground", new InstantCommand(() -> armSubystem.setArmState(ArmState.Ground)));
-    eventMap.put("middle", new InstantCommand(() -> armSubystem.setArmState(ArmState.Middle)));
-    eventMap.put("high", new InstantCommand(() -> armSubystem.setArmState(ArmState.High)));
+    eventMap.put(Constants.Auto.EVENT_MAP_INTAKE, new InstantCommand(() -> intakeSubsystem.intake()));
+    eventMap.put(Constants.Auto.EVENT_MAP_OUTAKE, new InstantCommand(() -> intakeSubsystem.outake()));
+    eventMap.put(Constants.Auto.EVENT_MAP_PRINT, new PrintCommand("It has crossed the threshold"));
+    eventMap.put(Constants.Auto.EVENT_MAP_STOWED, new InstantCommand(() -> armSubystem.setArmState(ArmState.Stowed)));
+    eventMap.put(Constants.Auto.EVENT_MAP_GROUND, new InstantCommand(() -> armSubystem.setArmState(ArmState.Ground)));
+    eventMap.put(Constants.Auto.EVENT_MAP_MIDDLE, new InstantCommand(() -> armSubystem.setArmState(ArmState.Middle)));
+    eventMap.put(Constants.Auto.EVENT_MAP_HIGH, new InstantCommand(() -> armSubystem.setArmState(ArmState.High)));
 
     AutoTrajectory testPath = new AutoTrajectory(driveSubsystem, "test_path");
 
